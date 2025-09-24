50ce13ebceebb8b0c084c98b76545ba3e8a4cbd0
EstevamTec <hestevan32@gmail.com>
2025-09-24 07:28:44 +0000

    comecei a estrutura

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,3 +4,23 @@ int main(){
     char Estado [50], Estado1 [50];
     char Codigo [50], Codigo1 [50];
     char Cidade [50], char Cidade1 [50];
+    
+/*Para usar Char sempre coloque [] e a quantidade que precisar usar ex: [50], se nao colocar 
+    não vai imprimir na hora de rodar o codigo.*/
+
+    int Populacao, Populacao1, Pontos_Turisticos, Pontos_Turisticos1;
+    float Km, Km1, PIB, PIB1;
+
+    // Cadastro primeira carta
+    
+    printf("Digite o Estado: \n");
+    scanf("%s", &Estado);
+
+    printf("Digite da Cidade: \n");
+    scanf("%s", &Cidade);
+
+    printf("Digite o codigo da carta: \n");
+    scanf("%s", &Codigo);
+
+    printf("Digite o numero populacional: \n");
+    scanf("%d", &Populacao);
